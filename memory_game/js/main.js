700f04c410d771f0b1cdbff1f46c29f7d6b91194
wickker <choydianchun@gmail.com>
2020-02-27 14:48:12 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,18 @@
-console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
   }
 }
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
